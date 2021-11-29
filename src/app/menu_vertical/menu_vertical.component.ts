b41563e1e4b4bc42b8a8a2822fce03fe3f538b89
Anna Sardone <sardone@artemat.it>
2021-11-29 18:30:28 +0100

    verticale

--- src/app/menu_vertical/menu_vertical.component.ts
+++ src/app/menu_vertical/menu_vertical.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu_vertical',
   templateUrl: './menu_vertical.component.html',
-  styleUrls: ['./menu_vertical.component.css']
+  styleUrls: ['./menu_vertical.component.css'],
 })
 export class Menu_verticalComponent implements OnInit {
+  lista: object[] = [
+    { name: 'Home', link: '#home' },
+    { name: 'Trasparenza Amministrativa', link: '#Trasparenza Amministrativa' },
+    { name: 'Bandi e Gare', link: '#Bandi e Gare' },
+    { name: 'Albo Pretorio', link: '#Albo Pretorio' }
 
-  constructor() { }
+  ];
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
